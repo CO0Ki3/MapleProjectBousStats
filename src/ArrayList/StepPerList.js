f6a3cca3ea9331ac8bb227bf8e9991fcba432257
co0ki3 <ehrrhgus0324@gmail.com>
2021-05-03 21:32:15 +0900

    Change Argument Integer to String

--- src/ArrayList/StepPerList.js
+++ src/ArrayList/StepPerList.js
@@ -1,15 +1,15 @@
 const EternalPercent = {
-  7: 0.01,
-  6: 0.25,
-  5: 0.45,
-  4: 0.29,
+  "7": 0.01,
+  "6": 0.25,
+  "5": 0.45,
+  "4": 0.29,
 };
 
 const PowerfulPercent = {
-  6: 0.14,
-  5: 0.36,
-  4: 0.3,
-  3: 0.2,
+  "6": 0.14,
+  "5": 0.36,
+  "4": 0.3,
+  "3": 0.2,
 };
 
 export { EternalPercent, PowerfulPercent };
