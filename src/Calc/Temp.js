2e2810f75c7cc7aed9e87ecc782a7f8ac49fd238
co0ki3 <ehrrhgus0324@gmail.com>
2021-05-14 20:54:59 +0900

    Temp

--- src/Calc/Temp.js
+++ src/Calc/Temp.js
@@ -7,15 +7,21 @@ import { OptionContext } from "../SelectOption/SelectBox";
 const Temp = () => {
   const data1 = useContext(ListContext);
   const data2 = useContext(SortContext);
-  const data3 = useContext(StepContext);
-  const data4 = useContext(OptionContext);
+  const data3 = useContext(OptionContext);
+  const data4 = useContext(StepContext);
 
+  const [stateData1, setStateData1] = useState();
 
-  console.log({ data1 });
+  if (!!data1 && stateData1 !== data1) {
+    setStateData1(data1);
+  }
+
+  console.log({ stateData1 });
   console.log({ data2 });
   console.log({ data3 });
   console.log({ data4 });
+// 
   return <></>;
-}
+};
 
 export default Temp;
