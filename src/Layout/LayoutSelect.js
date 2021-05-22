cdbd669c82e5239d25ec1c9452d90302cd326eda
co0ki3 <ehrrhgus0324@gmail.com>
2021-05-22 18:08:53 +0900

    Cover SelectDisplay with StoreProvider

--- src/Layout/LayoutSelect.js
+++ src/Layout/LayoutSelect.js
@@ -1,12 +1,17 @@
 import SelectDisplay from "../SelectOption/SelectDisplay";
 import SelectCompare from "../SelectOption/SelectCompare";
-import { SelectBoxStyle } from '../SelectOption/SelectStyle';
+import { SelectBoxStyle } from "../SelectOption/SelectStyle";
+import { StoreProvider } from "../hook";
 
 function LayoutSelect() {
   return (
     <main>
-      <SelectBoxStyle><SelectDisplay /></SelectBoxStyle>
-      <SelectCompare />
+      <StoreProvider>
+        <SelectBoxStyle>
+          <SelectDisplay />
+        </SelectBoxStyle>
+        <SelectCompare />
+      </StoreProvider>
     </main>
   );
 }
